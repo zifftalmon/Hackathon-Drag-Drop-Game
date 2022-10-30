93748ff8823b84854faf7e4b4f8eca283c2c2fa7
ziv talmon <zivtalmon89@gmail.com>
2022-10-30 16:41:54 +0200

    drag and drop events added to JS file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let boxOne = document.getElementById("monaLisa");
 let boxTwo = document.getElementById("mondrian");
 let boxThree = document.getElementById("renoir");
-let boxFour = document.getElementById("monaLisa");
+let boxFour = document.getElementById("caravaggio");
 
 
 boxOne.setAttribute("draggable", "true");
@@ -15,5 +15,29 @@ boxThree.addEventListener("dragstart", startDragging);
 boxFour.addEventListener("dragstart", startDragging);
 
 function startDragging (event) {
+    event.dataTransfer.setData("image", event.target.id)
     console.log("yes");
+}
+
+function modifyDropZones () {
+    const dropZones = document.querySelectorAll(".dropFrame");
+
+    for (const zone of dropZones) {
+        zone.addEventListener("dragover", draggingOver);
+        zone.addEventListener("drop", dropOnTarget);
+    }
+}
+
+modifyDropZones();
+
+function draggingOver(event) {
+    event.preventDefault();
+}
+
+function dropOnTarget (event) {
+    event.preventDefault();
+    event.target.style.background = "#1f5e4a";
+    const dataId = event.dataTransfer.getData("image");
+    const getImage =document.getElementById(dataId);
+    event.target.appendChild(getImage);
 }
